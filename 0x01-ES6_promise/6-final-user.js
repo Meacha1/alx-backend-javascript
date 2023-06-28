eb785ee653dafd1d5aa2ca788e83e8e198743d41
Meacha1 <meachattd@gmail.com>
2023-06-28 19:34:41 +0300

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,17 +1,14 @@
-import { signUpUser } from './4-user-promise.js';
-import { uploadPhoto } from './5-photo-reject.js';
+import { signUpUser } from './4-user-promise';
+import { uploadPhoto } from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
     .then((results) => {
-        {
-            return results.map((result) => {
-                return {
-                    status: result.status,
-                    value: result.status === 'fulfilled' ? result.value : result.reason,
-                }
-            })
-        }
+      return results.map((result) => {
+        return {
+          status: result.status,
+          value: result.status === 'fulfilled' ? result.value : result.reason,
+        };
+      });
     });
-};
-
+}

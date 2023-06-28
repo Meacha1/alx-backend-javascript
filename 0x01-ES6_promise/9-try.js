63adde4acf5c2078abb25d43b3f169eb00b84075
Meacha1 <meachattd@gmail.com>
2023-06-28 21:07:46 +0300

    Guardrail was processed

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -6,7 +6,8 @@ export default function guardrail(mathFunction) {
     return queue;
   } catch (error) {
     queue.push(error.toString());
+  } finally {
+    queue.push('Guardrail was processed');
   }
-  queue.push('Guardrail was processed');
   return queue;
 }

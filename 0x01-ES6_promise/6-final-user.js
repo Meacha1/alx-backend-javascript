ac5c0a94289f2e74f50bc13e529274f09f8fe731
Meacha1 <meachattd@gmail.com>
2023-06-28 19:47:05 +0300

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,15 @@
-import { signUpUser } from './4-user-promise';
-import { uploadPhoto } from './5-photo-reject';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
-    .then((results) => {
-      return results.map((result) => {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => ({
+      return results.map((result) => ({
         return {
           status: result.status,
           value: result.status === 'fulfilled' ? result.value : result.reason,
-        };
-      });
-    });
+        }
+      }))
+    }));
 }
+//7:7  error  Parsing error: Unexpected keyword 'return'

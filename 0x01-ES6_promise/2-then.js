aa05be9d0840f85499b1e8dc1f5683825025ea76
Meacha1 <meachattd@gmail.com>
2023-06-28 15:52:16 +0300

    2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,7 +2,7 @@ export default function handleResponseFromAPI(promise) {
   return promise.then(() => (
     {
       status: 200,
-      message: 'success',
+      body: 'success',
     }
   ))
     .catch(() => new Error())

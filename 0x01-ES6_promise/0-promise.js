515fbfa01b3db1b0954b39e0eb95a0cbc0e24a5b
Meacha1 <meachattd@gmail.com>
2023-06-28 11:41:12 +0300

    promise0_edited

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,11 @@
 function getResponseFromAPI() {
     return new Promise((resolve, reject) => {
+        // preform API request or asynchronous aperation
         setTimeout(() => {
             resolve("API response data");
         }, 2000);
     });
 }
+
 
 export default getResponseFromAPI;

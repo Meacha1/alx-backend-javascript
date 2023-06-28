cb7e765b0ccdbce3f0733c9de1bf7d26aa8ae449
Meacha1 <meachattd@gmail.com>
2023-06-28 12:45:09 +0300

    eslint

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,8 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        // preform API request or asynchronous aperation
-        setTimeout(() => {
-            resolve("API response data");
-        }, 2000);
-    });
+  return new Promise((resolve) => {
+    // preform API request or asynchronous aperation
+    setTimeout(() => {
+      resolve('API response data');
+    }, 2000);
+  });
 }

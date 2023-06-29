2bab23a29f53b05650dc5d359d0de07d973509c8
Meacha1 <meachattd@gmail.com>
2023-06-29 18:55:46 +0300

    Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,12 +1,12 @@
 export default class Building {
   constructor(sqft) {
     if (this.constructor !== Building && !this.evacuationWarningMessage) {
-      throw new Error('Class extending Building must implement evacuationWarningMessage');
+      throw new Error('Class extending Building must overrid evacuationWarningMessage');
     }
-    this._sqft = sqft
+    this._sqft = sqft;
   }
 
   get sqft() {
-    return this._sqft
+    return this._sqft;
   }
 }

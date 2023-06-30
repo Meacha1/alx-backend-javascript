4e7dc293eaed75d32899bf5231c3232ced7d98ea
Meacha1 <meachattd@gmail.com>
2023-06-30 14:24:02 +0300

    10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const i of array) {
-    const value = array[i];
-    array[i] = appendString + value;
+  for (let value of array) {
+    value = `${appendString}${value}`;
   }
 
   return array;

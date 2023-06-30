876d72fcfcaa9825c7c88a960366d51ae16adcf6
Meacha1 <meachattd@gmail.com>
2023-06-30 11:28:00 +0300

    task1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -4,8 +4,8 @@ export default function taskBlock(trueOrFalse) {
 
   if (trueOrFalse) {
     (function () {
-      task = true;
-      task2 = false;
+      const task = true;
+      const task2 = false;
     })();
   }
 

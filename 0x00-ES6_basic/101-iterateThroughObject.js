4bd821e5860b0c51944a1c39b88b27ca8d780ff8
Meacha1 <meachattd@gmail.com>
2023-06-30 16:24:32 +0300

    101

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,12 +1,4 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let result = '';
-
-  for (const employee of reportWithIterator) {
-    result += employee + ' | ';
-  }
-
-  // Remove the trailing ' | ' from the result
-  result = result.slice(0, -3);
-
-  return result;
+  const employeesArray = [...reportWithIterator];
+  return employeesArray.join(' | ');
 }

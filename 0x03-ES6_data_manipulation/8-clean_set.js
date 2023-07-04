500092c0381e78f3b508013677d40421091d33a1
Meacha1 <meachattd@gmail.com>
2023-07-04 13:14:24 +0300

    8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,12 @@
 export default function cleanSet(set, startString) {
-  if (!startString) {
-    return ''
+  if (startString.length === 0 || set.size === 0 || startString === undefined || startString === null || startString === '' || startString.length > 1) {
+    return '';
   }
-  const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
-  const cleanedValues = filteredValues.map((value) => value.slice(startString.length));
-  return cleanedValues.join('-');
+  if (startString.length > 0) {
+    const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
+    const cleanedValues = filteredValues.map((value) => value.slice(startString.length));
+    return cleanedValues.join('-');
+  }
+
+  return '';
 }

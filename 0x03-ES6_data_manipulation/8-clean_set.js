0781a337c820dc240eaaf6822723e9d95ab15171
Meacha1 <meachattd@gmail.com>
2023-07-04 13:25:40 +0300

    8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,19 @@
-export default function cleanSet(set, startString) {
-  return [...set]
-    .filter(item => item.startsWith(startString))
-    .map(item => item.slice(startString.length))
-    .join('-');
+function cleanSet(set, startString) {
+  if (startString.length === 0) {
+    return '';
+  }
+  const cleanstring = [];
+  if (startString.length > 0) {
+    set.forEach(item => {
+      if (item.startsWith(startString)) {
+        item = item.slice(startString.length);
+        cleanstring.push(item);
+      }
+    });
+  }
+  return [...cleanstring].join('-');
 }
+
+
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));

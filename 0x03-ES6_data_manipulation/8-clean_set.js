b801108473ea3dfea5fb6307c26e1445343d559a
Meacha1 <meachattd@gmail.com>
2023-07-04 13:05:39 +0300

    clean

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,11 @@
-export default function cleanSet(set, startString) {
+function cleanSet(set, startString) {
+  if (!startString) {
+    return ''
+  }
   const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
   const cleanedValues = filteredValues.map((value) => value.slice(startString.length));
   return cleanedValues.join('-');
 }
+
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));

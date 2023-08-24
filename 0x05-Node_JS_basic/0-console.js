0067c7b28eb92502412cf05cfa40e34622ee6e1e
Meacha1 <meachattd@gmail.com>
2023-08-24 21:49:06 +0300

    console.log

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,7 @@
 /**
  * Desplay a message in the console.
  * @param {string} message - The message to display.
- * @author Meacha Teshome
  */
-
-
 const displayMessage = (message) => {
     console.log(message);
 }
